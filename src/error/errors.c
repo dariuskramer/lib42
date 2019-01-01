81f72c420952c84af0ea35a9863c16d1cb6acc2c
dkrm <school@dkrm.fastmail.com>
2019-01-01 19:15:48 +0100

    error: avoid memory leak for error_set_context

--- src/error/errors.c
+++ src/error/errors.c
@@ -12,7 +12,7 @@
 #include "io_42.h"
 #include "error_42.h"
 
-static t_string		g_context;
+static char 		*g_context;
 static const char	*g_project_name;
 
 void	die(const char *msg)
@@ -24,32 +24,32 @@ void	die(const char *msg)
 void	error_register_project_name(const char *project_name)
 {
 	g_project_name = project_name;
-	if (string_init(&g_context) == NULL)
-		die(FATAL_MALLOC);
 }
 
 void	error_set_context(const char *format, ...)
 {
+	t_string	buf;
 	va_list		args;
 
 	assert(format != NULL);
 	va_start(args, format);
-	string_truncate(&g_context, 0);
-	if (ft_printf_fmt(&g_context, format, args) == NULL)
+	if (string_init(&buf) == NULL || ft_printf_fmt(&buf, format, args) == NULL)
 		die(FATAL_MALLOC);
+	g_context = buf.str;
 	va_end(args);
 }
 
 void	error_print(const char *msg)
 {
 	assert(g_project_name != NULL);
-	if (g_context.len == 0)
+	if (g_context == NULL)
 	{
 		ft_dprintf(2, "%s: %s\n", g_project_name, msg);
 	}
 	else
 	{
-		ft_dprintf(2, "%s: %s: %s\n", g_project_name, msg, g_context.str);
-		string_shutdown(&g_context);
+		ft_dprintf(2, "%s: %s: %s\n", g_project_name, msg, g_context);
+		free(g_context);
+		g_context = NULL;
 	}
 }
