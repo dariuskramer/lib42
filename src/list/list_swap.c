774b87678951927092816b79b1dc310732417a1e
dkrm <school@dkrm.fastmail.com>
2019-01-05 12:22:25 +0100

    list: fix bad pointers in list_swap

--- src/list/list_swap.c
+++ src/list/list_swap.c
@@ -4,9 +4,9 @@
 
 t_list	*list_swap(t_list *list, size_t e1, size_t e2)
 {
-	t_list_node	*node1;
-	t_list_node	*node2;
-	void		*tmp;
+	void	*data1;
+	void	*data2;
+	void	*tmp;
 
 	if (e1 >= list->len || e2 >= list->len)
 		return (NULL);
@@ -15,11 +15,11 @@ t_list	*list_swap(t_list *list, size_t e1, size_t e2)
 	tmp = pool_acquire_block(&list->pool);
 	if (tmp == NULL)
 		return (NULL);
-	node1 = list_get_at(list, e1);
-	node2 = list_get_at(list, e2);
-	ft_memcpy(tmp, LIST_NODE_DATA(node1), list->elem_size);
-	ft_memcpy(LIST_NODE_DATA(node1), LIST_NODE_DATA(node2), list->elem_size);
-	ft_memcpy(LIST_NODE_DATA(node2), tmp, list->elem_size);
+	data1 = list_get_at(list, e1);
+	data2 = list_get_at(list, e2);
+	ft_memcpy(tmp, data1, list->elem_size);
+	ft_memcpy(data1, data2, list->elem_size);
+	ft_memcpy(data2, tmp, list->elem_size);
 	pool_release_block(list->pool, tmp);
 	return (list);
 }
